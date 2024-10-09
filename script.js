dda0070c529d56fc0eaaa1d1c23cecec774e5fdc
rijulnanda <31491803+rijulnanda@users.noreply.github.com>
2024-10-09 16:17:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyC469sIwL2Ja5fIu4wC4yWotuXdZApNx3s",
+  authDomain: "supplysynctest.firebaseapp.com",
+  databaseURL: "https://supplysynctest-default-rtdb.firebaseio.com",
+  projectId: "supplysynctest",
+  storageBucket: "supplysynctest.appspot.com",
+  messagingSenderId: "153630325231",
+  appId: "1:153630325231:web:f5c143dd4fc72fc301de1a",
+  measurementId: "G-Q5FQREPD8H"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
